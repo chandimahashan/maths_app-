a5547ce544c8a68b47c107eb64621b62e29055f1
Chandima Hashan <chandima@agrithmics.lk>
2024-02-29 11:21:14 +0530

    1st commit

--- app/src/main/java/com/example/cardviewtstapp/Perimeter.kt
+++ app/src/main/java/com/example/cardviewtstapp/Perimeter.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 class Perimeter : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_perimeter)
     }
 }
